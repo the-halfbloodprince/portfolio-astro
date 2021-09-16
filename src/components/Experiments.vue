e948ea237e7b7ed1dca9c5ae323590d03bb940b5
Manish Kumar Das <manish.das@krishi.network>
2021-09-17 01:34:58 +0530

    completed the resume.vue

---
+++ src/components/Experiments.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="h-screen px-8 xl:px-0 py-16 relative flex flex-col justify-center space-y-8 resume">
+		<div class="text-6xl text-accent">{{title}}</div>
+		<div class="section1">
+			
+		</div>
+	</div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+	export default {
+		setup() {
+			const title = "Okay but is it all just the usual stuff?"
+			const catchPhrase = "My Official Documentation"
+			const resume = {
+				education: [
+					{
+						course: "Integrated Masters in Applied Geology",
+						school: "Indian Institute of Technology, Kharagpur",
+						courseDuration: "(2019-2024)",
+					},
+					{
+						course: "Higher Secondary Education in Science",
+						school: "KV NEIST",
+						courseDuration: "(2016-2018)",
+					}
+				],
+				experience: [
+					{
+						jd: "Software Development Intern (Full Stack)",
+						organisation: "Krishi Network",
+						jobDuration: "(July 2021-Sept 2021)",
+					},
+					{
+						jd: "Research Intern",
+						organisation: "SSERD",
+						jobDuration: "(Oct 2019-Nov 2019)",
+					},
+				]
+			}
+
+			// console.log(resume)
+
+			return {title, resume, catchPhrase}
+		}
+	}
+</script>
--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,20 +1,33 @@
 <template>
-	<div class="h-screen px-8 xl:px-0 py-16 relative flex flex-col justify-center space-y-8">
+	<div class="h-screen px-8 xl:px-0 py-16 relative flex flex-col justify-center space-y-8 resume">
 		<h1 class="text-6xl text-accent">Resume</h1>
 		<h1 class="text-2xl text-white">{{catchPhrase}}</h1>
-		<div class="">
-			<div v-for="(_, cat, index) in resume">
+		<div class="supreme">
+			<div class="line"></div>
+			<div v-for="(_, cat, __) in resume">
 				<div class="resumeCategoryBox">
 					<div class="categoryBox">
 						<div class="category">{{cat}}</div>
 					</div>
 					<div class="contentBox">
-						<div v-for="(content, _, index) in resume[cat]">
+						<div v-for="(content, index2) in resume[cat]">
 							<div class="v">
-								<div v-for="(detail, _, index) in content">
-									<div v-if="cat === 'education' && _ === 'school' ">from <b>{{detail}}</b></div>
-									<div v-else-if="cat === 'experience' && _ === 'organisation' ">at <b>{{detail}}</b></div>
-									<div v-else>{{detail}}</div>
+								<div class="list-icon">
+									<div v-if="index2===0">
+										<img src="assets/latestExp.svg" class="list-icon" />
+									</div>
+									<div v-else>
+										<img src="assets/oldExp.svg" class="list-icon" />
+									</div>
+								</div>
+								<div class="list-content">
+									<div v-for="(detail, _, index) in content">
+										<div class="l1" v-if="index===0">{{detail}}</div>
+
+										<div class="l2" v-else-if="cat === 'education' && _ === 'school' ">from <span class="special">{{detail}}</span></div>
+										<div class="l2" v-else-if="cat === 'experience' && _ === 'organisation' ">at <span class="special">{{detail}}</span></div>
+										<div class="l2" v-else>{{detail}}</div>
+									</div>
 								</div>
 							</div>
 						</div>
@@ -32,6 +45,23 @@
 
 <style>
 
+	.resume {
+		margin-bottom: 7rem;
+	}
+
+	.supreme {
+		position: relative;
+	}
+
+	.l1 {
+		font-size: 1.7rem;
+	}
+
+	.l2 {
+		font-size: 1.1rem;
+		color: #999;
+	}
+
 	.resumeCategoryBox {
 		display: flex;
 		width: 100%;
@@ -40,19 +70,29 @@
 	}
 
 	.categoryBox {
-		width: 40%;
+		width: 35%;
 		text-transform: capitalize;
 		font-weight: bold;
 		font-size: 2rem;
 	}
 
 	.contentBox {
-		width: 60%;
+		width: 65%;
 		font-size: 1.3rem;
 	}
 
 	.v {
 		margin-bottom: 1rem;
+		display: flex;
+	}
+
+	.list-content {
+		margin-left: 1rem;
+	}
+
+	.list-icon {
+		width: 3rem;
+		z-index: 3;
 	}
 
 	.button {
@@ -69,12 +109,24 @@
 		color: #22223B;
 		border-radius: 10px;
 	}
+
+	.line {
+		content: " ";
+		height: 100%;
+		width: 4px;
+		/* background-color: rgb(146, 109, 180); */
+		background:linear-gradient(to bottom,  rgb(146, 109, 180), #00041E);
+		position: absolute;
+		left: calc(35% + 1.5rem - 2px);
+		top: .5rem;
+	}
 </style>
 
 <script>
 	export default {
 		setup() {
 			const catchPhrase = "My Official Documentation"
+			const dummy = undefined;
 			const resume = {
 				education: [
 					{
